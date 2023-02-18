6a3037bf7a18c768bef73f74926dc8a01c7aabe7
Seacow <46582003+SeacowX@users.noreply.github.com>
2023-02-18 13:27:46 -0500

    upload

--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -5,6 +5,9 @@ from torch.optim.lr_scheduler import _LRScheduler
 
 
 class CustomLRScheduler(_LRScheduler):
+    '''
+    docstring for CustomLRScheduler
+    '''
     def __init__(self, optimizer, T_max, eta_min=0, last_epoch=-1):
         """
         Create a new scheduler.
@@ -18,6 +21,9 @@ class CustomLRScheduler(_LRScheduler):
         super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
 
     def get_lr(self) -> List[float]:
+        '''
+        docstring for get_lr
+        '''
         # Note to students: You CANNOT change the arguments or return type of
         # this function (because it is called internally by Torch)
 
